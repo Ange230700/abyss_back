45211b48b57d0709b51cc1a7ace257c3d95a0182
Ange230700 <kouakouangeericstephane@gmail.com>
2025-07-12 12:47:23 +0200

    chore: do stuff

---
+++ src/app.module.ts
@@ -0,0 +1,12 @@
+// src\app.module.ts
+
+import { Module } from '@nestjs/common';
+import { AppController } from '~/src/app.controller';
+import { AppService } from '~/src/app.service';
+
+@Module({
+  imports: [],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
--- test/records.e2e-spec.ts
+++
@@ -1,1 +0,0 @@
-// test\records.e2e-spec.ts
