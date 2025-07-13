9f936337c705cfb445b7ff3edc87fe6559c41657
Ange230700 <kouakouangeericstephane@gmail.com>
2025-07-13 10:26:24 +0200

    test: write 'em

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,1 +1,29 @@
 // test\app.e2e-spec.ts
+
+import { Test, TestingModule } from '@nestjs/testing';
+import { INestApplication } from '@nestjs/common';
+import * as request from 'supertest';
+import { AppModule } from '~/src/app.module';
+
+describe('AppController (e2e)', () => {
+  let app: INestApplication;
+
+  beforeAll(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+
+    app = moduleFixture.createNestApplication();
+    await app.init();
+  });
+
+  afterAll(async () => {
+    await app.close();
+  });
+
+  it('GET / should return "Hello World!"', async () => {
+    const res = await request(app.getHttpServer()).get('/');
+    expect(res.status).toBe(200);
+    expect(res.text).toBe('Hello World!');
+  });
+});
