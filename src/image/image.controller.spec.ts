f33e9a9cf29aa0ec7c1986740c6d2f168d752a7e
Ange230700 <kouakouangeericstephane@gmail.com>
2025-07-14 08:02:48 +0200

    chore: do stuff

--- src/image/image.controller.spec.ts
+++ src/image/image.controller.spec.ts
@@ -1,5 +1,6 @@
 // src\image\image.controller.spec.ts
 
+import { faker } from '@faker-js/faker';
 import { Test, TestingModule } from '@nestjs/testing';
 import { ImageController } from '~/src/image/image.controller';
 import { ImageService } from '~/src/image/image.service';
@@ -32,7 +33,10 @@ describe('ImageController', () => {
   });
 
   it('should call service.create on create()', async () => {
-    const dto = { id_furniture: 1, url: 'https://example.com' };
+    const dto = {
+      id_furniture: faker.number.int({ min: 1, max: 99 }),
+      url: faker.image.url(),
+    };
     service.create.mockResolvedValue('created');
     const result = await controller.create(dto as any);
     expect(service.create).toHaveBeenCalledWith(dto);
@@ -40,31 +44,35 @@ describe('ImageController', () => {
   });
 
   it('should call service.findAll on findAll()', async () => {
-    service.findAll.mockResolvedValue(['img1', 'img2']);
+    const fakeArray = [faker.lorem.word(), faker.lorem.word()];
+    service.findAll.mockResolvedValue(fakeArray);
     const result = await controller.findAll();
     expect(service.findAll).toHaveBeenCalled();
-    expect(result).toEqual(['img1', 'img2']);
+    expect(result).toEqual(fakeArray);
   });
 
   it('should call service.findOne on findOne()', async () => {
+    const id = faker.number.int({ min: 1, max: 999 });
     service.findOne.mockResolvedValue('found');
-    const result = await controller.findOne('9');
-    expect(service.findOne).toHaveBeenCalledWith(9);
+    const result = await controller.findOne(id.toString());
+    expect(service.findOne).toHaveBeenCalledWith(id);
     expect(result).toBe('found');
   });
 
   it('should call service.update on update()', async () => {
-    const dto = { url: 'updated' };
+    const id = faker.number.int({ min: 1, max: 999 });
+    const dto = { url: faker.image.url() };
     service.update.mockResolvedValue('updated');
-    const result = await controller.update('4', dto as any);
-    expect(service.update).toHaveBeenCalledWith(4, dto);
+    const result = await controller.update(id.toString(), dto as any);
+    expect(service.update).toHaveBeenCalledWith(id, dto);
     expect(result).toBe('updated');
   });
 
   it('should call service.remove on remove()', async () => {
+    const id = faker.number.int({ min: 1, max: 999 });
     service.remove.mockResolvedValue('deleted');
-    const result = await controller.remove('10');
-    expect(service.remove).toHaveBeenCalledWith(10);
+    const result = await controller.remove(id.toString());
+    expect(service.remove).toHaveBeenCalledWith(id);
     expect(result).toBe('deleted');
   });
 });
